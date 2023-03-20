79bfe16017cfd4ec2be62bce5d5659702fc904a9
Rahul Trehan <rtrehan@rtrehan-ltm.internal.salesforce.com>
2023-03-20 22:41:03 +0800

    working v2.4 aws version

--- src/App.js
+++ src/App.js
@@ -13,15 +13,13 @@ import { Config } from "./tableau/config.js";
 import { ChatRes } from "./lib/chatres.js";
 import { Worksheet } from "./tableau/worksheets.js";
 import { getSummaryData } from "./tableau/getData.js";
-
+import Link from "@mui/material/Link";
 
 const tableau = window.tableau;
 
 const App = observer(function () {
-
-  const [worksheets, setWorksheets] =useState([]);
+  const [worksheets, setWorksheets] = useState([]);
   const [selectedData, setSelectedData] = useState([]);
-
 
   const [summaryData, setSummaryData] = useState([]);
 
@@ -32,72 +30,66 @@ const App = observer(function () {
       setWorksheets(tableau.extensions.dashboardContent.dashboard.worksheets);
 
       //setSummaryData (getSummaryData(selectedSheet))
-
-
     });
-  },[]);
+  }, []);
 
-  const[config, setConfig]= useState({});
-  const [showConfig, setShowConfig] = useState(true)
+  const [config, setConfig] = useState({});
+  const [showConfig, setShowConfig] = useState(true);
 
-  function selectedDataCallback(selectedData){
+  //to get the summary data back from worksheet component
+  function selectedDataCallback(selectedData) {
     console.log("setting callback selected data", selectedData);
-      setSelectedData(selectedData)
+    setSelectedData(selectedData);
   }
 
-  function configCallback(c){
-
+  function configCallback(c) {
     console.log("config callback receieved..", c);
 
-     //setConfig({...config,key:c.key,url:c.url,model:c.model, temperature:c.temperature});
-      setConfig({...c})
-      
-      setShowConfig(c.key.includes('enter your key'))
-     // console.log("config callback set..", config);
+    //setConfig({...config,key:c.key,url:c.url,model:c.model, temperature:c.temperature});
+    setConfig({ ...c });
+
+    setShowConfig(c.key.includes("enter your key"));
+    // console.log("config callback set..", config);
   }
-
-
-
 
   //return <ChatSelected />
   return (
     <Fragment>
       <div className="container-fluid bg-secondary text-light ">
-      <div className="row bg-secondary text-light  justify-content-end">
-        <div className="col-6 col-sm-6 ">
-          <h6>Tableau Chat GPT Advisor Extension</h6>
-         
+        <div className="row bg-secondary text-light  justify-content-end">
+          <div className="col-7 col-sm-6 ">
+            <div className="row align-content-start">
+              <div className="col-7">Tableau Chat GPT Advisor</div>
+          <div className="col-5">
+          <Link  sx={{ color: "white"}}
+                href="https://www.linkedin.com/in/trehanrahul/"
+                target="_blank"
+              >
+                @Rahul Trehan
+              </Link>
+          </div>
+             
+            </div>
+          </div>
+
+          <div className="col-5 col-sm-6 ps-3">
+            <Config configCallback={configCallback} />
+          </div>
         </div>
-        <div className="col-6 col-sm-6">
-        <Config configCallback={configCallback}/>
+
+        <p></p>
+        <div className="row bg-secondary text-light  ">
+          <Worksheet
+            worksheets={worksheets}
+            selectedDataCallback={selectedDataCallback}
+          />
         </div>
-       
+        <p></p>
+
+        <ChatSelected />
       </div>
-
-<p></p>
-    <div className="row bg-secondary text-light  ">
- 
-    <Worksheet worksheets={worksheets} selectedDataCallback={selectedDataCallback}/>
-   
-
-    </div>
-    <p></p>
-  
-       <ChatSelected />
-
-
-  </div>
-
-      
-  
-
     </Fragment>
   );
-}
-
-
-);
-
-
+});
 
 export default App;
