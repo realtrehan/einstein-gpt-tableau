b5f03630b1248fbef3139cb6a899f4367da40ef3
Rahul Trehan <rtrehan@rtrehan-ltm.internal.salesforce.com>
2023-03-21 09:09:50 +0800

    aws v2.5

--- src/tableau/getData.js
+++ src/tableau/getData.js
@@ -1,20 +1,15 @@
-
-
-
 import { gptMessages } from "../context";
-
 
 export const getSummaryData = async function getSummaryData(selectedSheetName) {
   const tableau = window.tableau;
 
   //initialize the new messages to zero for each worksheet in the gptmessage store
-const newMsgs = [];
-gptMessages.messages.clear() ; //this is needed to empty the store array for each worksheet selection
-
+  const newMsgs = [];
+  gptMessages.messages.clear(); //this is needed to empty the store array for each worksheet selection
 
   //  return new Promise((resolve, reject) => {})
 
-  console.log("get summary caalled for shheet...", selectedSheetName)
+  console.log("get summary caalled for shheet...", selectedSheetName);
 
   let formattedTable = []; //this variable is used to store the crosstab extracted form the worksheet summary data
 
@@ -46,14 +41,11 @@ gptMessages.messages.clear() ; //this is needed to empty the store array for eac
     row.forEach(function (val) {
       // console.log("_nativeValue type is ", typeof val._nativeValue);
       if (typeof val._nativeValue === "number") {
-
         if (val._nativeValue > 1) {
-            formattedRow.push(parseFloat(val._nativeValue).toFixed(2));
+          formattedRow.push(parseFloat(val._nativeValue).toFixed(2));
+        } else {
+          formattedRow.push(parseFloat(val._nativeValue).toFixed(4));
         }
-        else{
-            formattedRow.push(parseFloat(val._nativeValue).toFixed(4));
-        }
-        
       } else {
         formattedRow.push(val._nativeValue);
       }
@@ -72,11 +64,18 @@ gptMessages.messages.clear() ; //this is needed to empty the store array for eac
 
   console.log("returning formatted data..", formattedTable);
 
-    //push the summary data to chatGPT messages array
-  newMsgs.push({ role: "user", content: "the data for analysis has these columns: "+ formattedTable[0]  });
-  newMsgs.push({ role: "user", content: "the data set values are : "+ formattedTable.slice(2)  });
-_.assign(gptMessages.messages, newMsgs);
-// gptMessages.messages.replace(newMsgs)
+  //push the summary data to chatGPT messages array
+  newMsgs.push({ role: "system", content: "You are a helpful data analyst" });
+  newMsgs.push({
+    role: "user",
+    content: "the data for analysis has these columns: " + formattedTable[0],
+  });
+  newMsgs.push({
+    role: "user",
+    content: "the data set values are : " + formattedTable.slice(2),
+  });
+  _.assign(gptMessages.messages, newMsgs);
+  // gptMessages.messages.replace(newMsgs)
 
   return formattedTable;
 };
