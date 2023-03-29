6413d1032e336e34c4bbf3c81cc14e63d18a3ca0
Rahul Trehan <rtrehan@rtrehan-ltm.internal.salesforce.com>
2023-03-29 14:59:10 +0800

    with edit chat functionality

--- src/chats.js
+++ src/chats.js
@@ -2,56 +2,85 @@ import {
   useChats,
   useChatsDispatch,
   useChatsDetailsDispatch,
-  gptResponses
+  gptResponses,
 } from "./context.js";
 
 import { ChatsDetailsList } from "./chatDetails.js";
 import { Fragment, useState } from "react";
 import React, { StrictMode } from "react";
 import { Observer, observer } from "mobx-react";
-import { initialChats  as chats} from "./context.js";
+import { initialChats as chats } from "./context.js";
+import EditOutlinedIcon from "@mui/icons-material/EditOutlined";
 
-
-
-export const ChatsList = observer( function ({ selectChat }) {
- // const chats = useChats();
- // const chatsDispatch = useChatsDispatch();
+export const ChatsList = observer(function ({ selectChat }) {
+  // const chats = useChats();
+  // const chatsDispatch = useChatsDispatch();
   //const chatsDetailsDispatch = useChatsDetailsDispatch();
 
-  console.log("chat list triggereed..", chats)
+  const [editChatID, setEditChatID] = useState(null);
+  const [inputVal, setInputVal] = useState(null);
 
+  console.log("chat list triggereed..", chats);
 
-  const Chat = observer( function ({ chat }) {
+  const Chat = observer(function ({ chat }) {
     const [hover, setHover] = useState("");
-    const [highlighted, setHighlighted] = useState("")
+    const [highlighted, setHighlighted] = useState("");
 
     return (
-   
-      <div
+      <div className="row p-1">
+        <div className="col-9">
+          <div
+            className={chat.active ? "highlighted" : `${highlighted} ${hover}`}
+            id={chat.id}
+            onClick={(e) => {
+              setHighlighted("highlighted");
+              buttonClicked(e);
+            }}
+            onMouseEnter={() => setHover("hover " + highlighted)}
+            onMouseLeave={() => setHover("" + highlighted)}
+          >
+            {chat.text}
+          </div>
+        </div>
 
-        className={ chat.active? "highlighted": `${highlighted} ${hover}`}
-        id={chat.id}
-        onClick={(e) => {
-            setHighlighted("highlighted")
-          buttonClicked(e);
-        }}
-        onMouseEnter={() => setHover("hover " + highlighted)}
-        onMouseLeave={() => setHover("" + highlighted)}
-      >
-        {chat.text}
+        <div id={chat.id} onClick={(e) => editChatName(e)} className="col-3">
+          <EditOutlinedIcon id={chat.id} onClick={(e) => editChatName(e)} />
+        </div>
       </div>
-     
     );
-  })
-  
+  });
+
+  function editChatName(e) {
+    console.log("selected edit chat id..", e.target.id);
+
+    setEditChatID(e.target.id);
+  }
+
+  //for the edit chat name enter key
+  function editChatEnter(e) {
+    console.log("enter edit chat submit..", e);
+    if (e.key === "Enter") {
+      chats.chats.forEach((chat) => {
+        if (chat.id == editChatID) {
+          chat.text = inputVal;
+        } //update the chat array with new name;
+        const newChatArray = _.cloneDeep(chats.chats);
+        _.assign(chats.chats, newChatArray); //updat the mobx store
+      });
+
+      setEditChatID(null); //set the edit state back to null;
+    }
+  }
 
   function buttonClicked(e) {
     console.log("selected chat id..", e.target.id);
 
     selectChat(e.target.id);
 
-    chats.chats.forEach(chat => { chat.id == e.target.id ? chat.active = true : chat.active=false })
-/** 
+    chats.chats.forEach((chat) => {
+      chat.id == e.target.id ? (chat.active = true) : (chat.active = false);
+    });
+    /** 
     chatsDispatch({
       type: "changed",
       id: e.target.id
@@ -59,17 +88,26 @@ export const ChatsList = observer( function ({ selectChat }) {
     */
   }
 
+  //<Chat id={chat.id} chat={chat} /> chat={gptResponses.responses[0].response}
 
-//<Chat id={chat.id} chat={chat} /> chat={gptResponses.responses[0].response}
-
-return (
-  <div>
-  {chats.chats.map((chat) => (
-    <Chat id={chat.id} chat={chat} />
-  ))}
-  <p></p>
-</div>
-)
-  }
-
-)
+  return (
+    <div>
+      {chats.chats.map((chat) =>
+        editChatID == chat.id ? (
+          <input
+            type="text"
+            class="form-control "
+            placeholder="rename chat..press enter to save"
+            onKeyPress={editChatEnter}
+            onChange={(e) => {
+              setInputVal(e.target.value);
+            }}
+          />
+        ) : (
+          <Chat id={chat.id} chat={chat} />
+        )
+      )}
+      <p></p>
+    </div>
+  );
+});
